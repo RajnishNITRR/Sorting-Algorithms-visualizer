d9912e897a110ba9ada39e1d5fe12f7b5389fafd
Rajnish Kumar <rajnish1800084@gmail.com>
2024-07-24 22:44:04 +0530

    initial commit

---
+++ src/SortingVisualizer/Visualizer.jsx
@@ -0,0 +1,9 @@
+import { useState , useEffect } from "react";
+import { bubbleSort } from "../algorithms/bubblesort";
+
+
+
+// Random Number Genrator
+const generateRandomNumber = (i, j) => {
+	return Math.floor(i + Math.random() * (j - i));
+};
