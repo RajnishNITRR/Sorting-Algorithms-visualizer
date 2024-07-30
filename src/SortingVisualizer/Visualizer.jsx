852fa3b6276fb0bc7c71cd5c550082363a77f8cc
Rajnish Kumar <rajnish1800084@gmail.com>
2024-07-30 23:49:44 +0530

    update sorting Visualizer file

--- src/SortingVisualizer/Visualizer.jsx
+++ src/SortingVisualizer/Visualizer.jsx
@@ -1,9 +1,19 @@
-import { useState , useEffect } from "react";
-import { bubbleSort } from "../algorithms/bubblesort";
+import React, { useState, useEffect } from 'react';
+import colors from './colorCodes';
+import GithubIcon from '../Icons/GithubIcon';
+import { mergeSortAnimation } from '../algorithms/mergesort';
+import { insertionSort } from '../algorithms/insertion';
+import { selectionSort } from '../algorithms/selectionsort';
+import { bubbleSort } from '../algorithms/bubblesort';
+import { quicksort } from '../algorithms/quicksort';
+import { heapsort } from '../algorithms/heapsort';
+// stylesheet
+import './SortingVisualizer.css';
 
 
 
-// Random Number Genrator
+
+// Random Number Generator
 const generateRandomNumber = (i, j) => {
 	return Math.floor(i + Math.random() * (j - i));
 };
